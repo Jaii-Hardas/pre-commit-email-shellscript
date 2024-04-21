ba69fba0a389e695affd4c98561a433a93bdb216
Jaii-Hardas <jaihardas1515@dnyanyog.org>
2024-04-21 23:00:01 +0530

    feat: Implement pre-commit-check-shellscript
    * Add product.java class
    * Add .pre-commit-config.yaml file

---
+++ check_email_domain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+expectedEmail="jaihardas1515@dnyanyog.org"
+
+email=$(git config user.email)
+
+if [ "$email" = "$expectedEmail" ]; then
+    echo "You are using email as per configuration, proceeding to commit"
+    exit 0
+else
+    echo "Configured email is not as per config. It should be $expectedEmail"
+    exit 1
+fi
